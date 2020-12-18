4df04b4fd17ee178d0c86214427dd5ba7152d0ad
Kimbelly Nguyen <Kimchi@Kimbellys-MacBook-Pro.local>
2020-12-18 17:20:46 -0500

    5 stickies

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
+// wraps my code
 window.onload = function(){ 
-    // wraps my code
 
 
-//drop down content
+// collect all the divs
+var divs = document.getElementsByClassName('draggable');
+// get max width and height
 
+// var winWidth = window.innerWidth;
+// var winHeight = window.innerHeight;
+
+var box = document.querySelector('.container');
+var maxWidth = box.clientWidth - 350;
+console.log("max width: " + maxWidth);
+var maxHeight = box.clientHeight - 200;
+console.log("max height: " + maxHeight);
+
+// iterates through each div
+for ( var i=0; i < divs.length; i++ ) {
+ 	
+    // the current div in the list
+    var thisDiv = divs[i];
+    console.log("div: " + i);
+    
+    var posx = Math.round(Math.random() * maxWidth);
+    console.log("posx: " + posx);
+    var posy = Math.round(Math.random() * maxHeight);
+    console.log("posy: " + posy);
+
+    // update top and left position
+    thisDiv.style.left = posx +"px";
+    thisDiv.style.top = posy +"px";
+    
+}
+
+
+// jquery draggable
+$( function() {
+    $( ".draggable" ).draggable({ stack: ".draggable" });
+  } );
+
+// smooth drop down
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -17,50 +53,6 @@ for (i = 0; i < coll.length; i++) {
       content.style.maxHeight = content.scrollHeight + "px";
     }
   });
-}
-
-// Make the DIV element draggable:
-dragElement(document.getElementById("stickie"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById("stickie")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById("stickie").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
 }
 
   
